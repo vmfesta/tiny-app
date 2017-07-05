b413f484be75884809638118bf50eb22f0c0fb27
vmfesta <vmfesta@gmail.com>
2017-07-05 12:33:54 -0400

    added login form, cookies, and logout button, all working

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 var express = require("express");
+var cookieParser = require('cookie-parser')
 var app = express();
+app.use(cookieParser())
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 var PORT = process.env.PORT || 8080;
@@ -7,8 +9,11 @@ var PORT = process.env.PORT || 8080;
 app.set("view engine", "ejs");
 
 var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  urls: {
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xK": "http://www.google.com"
+  },
+  username: ""
 };
 
 app.post("/urls/:id/update", (req, res) => {
@@ -18,8 +23,17 @@ app.post("/urls/:id/update", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body);
+  res.redirect("/urls");
+});
+
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { 
+    urls: urlDatabase,
+    username:req.cookies['username']
+  };
+  console.log(templateVars);
   res.render("urls_index", templateVars);
 });
 
@@ -34,9 +48,8 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log("Here");
-  var url = req.body
-  var short = generateRandomString();
+  let url = req.body
+  let short = generateRandomString();
   urlDatabase[short] = url.longURL;
   res.redirect(`http://localhost:8080/urls/${short}`);
 });
@@ -44,9 +57,19 @@ app.post("/urls", (req, res) => {
 app.post("/urls/:id/delete", (req, res) => {
   var short = req.params.id;
   delete urlDatabase[short];
-  res.redirect("/urls");s
+  res.redirect("/urls");
 });
 
+app.post("/urls/redirect/:id", (req, res) => {
+  var short = req.params.id;
+  res.redirect(`/urls/${short}`);
+});
+
+app.post("/logout", (req, res) => {
+  urlDatabase.username = "";
+  res.clearCookie('username');
+  res.redirect(`/urls/`);
+});
 
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
