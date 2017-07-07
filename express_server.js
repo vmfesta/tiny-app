38d9dbbb8e8d126d9b3e67f0e0480ce6e84fd991
vmfesta <vmfesta@gmail.com>
2017-07-07 11:55:31 -0400

    added cookie session, fixed some bugs, removed unused code

--- express_server.js
+++ express_server.js
@@ -1,8 +1,17 @@
 var express = require("express");
 const bcrypt = require('bcrypt');
-var cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+var cookieSession  = require('cookie-session');
+var Cookies = require( "cookies" );
 var app = express();
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: ["userId"],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 var PORT = process.env.PORT || 8080;
@@ -37,9 +46,10 @@ const users = {
   }
 };
 
+// "/" blank path redirects to urls list if logged or login if not
 app.get("/", (req, res) => {
-  const id = req.cookies['user_id'];
-  let user = searchUsername(id)
+  const id = req.session['user_id'];
+  let user = searchUser(id)
   if(user) {
     res.redirect("/urls");
   } else {
@@ -47,31 +57,35 @@ app.get("/", (req, res) => {
   }
 });
 
+//POST action that receives a short ID and update the long URL
 app.post("/urls/:id/update", (req, res) => {
   let short = req.params.id
   let users = urlDatabase.urls.users;
-  urlDatabase.urls[short].long = req.body.longURL;
+  urlDatabase.urls[short].long = {longURL: req.body.longURL};
   res.redirect("/urls");
 });
 
+//render login page
 app.get("/login", (req, res) => {
   res.render("login");
 });
 
+//POST action to login de user
 app.post("/login", (req, res) => {
   var auth = validateLogin(req.body.email, req.body.password);
   if(auth.valid) {
-    res.cookie('user_id', auth.id);
+    req.session["user_id"] = auth.id;
     res.redirect("/urls");
   }
   res.statusCode = 403;
   res.redirect("/login");
 });
 
+//render de urls list if logged if not goes to register page
 app.get("/urls", (req, res) => {
-  const id = req.cookies["user_id"];
+  const id = req.session["user_id"];
   if(id) {
-    let user = searchUsername(id);
+    let user = searchUser(id);
     let urls = urlsForUser(user.id);
     let templateVars = { 
       urls,
@@ -83,12 +97,11 @@ app.get("/urls", (req, res) => {
   }
 });
 
+//render page to create a new url if looged if not redirects to login
 app.get("/urls/new", (req, res) => {
-  // console.log(req.cookies['user_id']);
-  // debugger;
-  if(req.cookies['user_id']) {
-    const id = req.cookies['user_id'];
-    let user = searchUsername(id)
+  if(req.session['user_id']) {
+    const id = req.session['user_id'];
+    let user = searchUser(id)
     templateVars = { user: user };
     res.render("urls_new", templateVars);
   } else {
@@ -96,27 +109,27 @@ app.get("/urls/new", (req, res) => {
   }
 });
 
-
+//receives the short url and show the url page to the user be able to edit it
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id };
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  debugger;
   let url = req.body
   let short = generateRandomString();
-  let id = req.cookies['user_id'];
+  let id = req.session['user_id'];
   let link = {
     short: short,
     long: url,
-    user: [id]
+    users: [id]
   }
   urlDatabase.urls[short] = link;
 
   res.redirect(`http://localhost:8080/urls/${short}`);
 });
 
+//POST action to delete the URL if the user clicks on delete
 app.post("/urls/:id/delete", (req, res) => {
   console.log(req.params);
   var short = req.params.id;
@@ -124,26 +137,26 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-app.post("/urls/redirect/:id", (req, res) => {
-  var short = req.params.id;
-  res.redirect(`/urls/${short}`);
-});
-
+//POST action to logout user and destroy de cookie session
 app.post("/logout", (req, res) => {
   urlDatabase.username = "";
-  res.clearCookie('user_id');
+  req.session = null;
   res.redirect(`/login`);
 });
 
+//redirects the user to the full url page
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase.urls[req.params.shortURL].long;
   res.redirect(longURL);
 });
 
+//goes to the register page
 app.get("/register", (req, res) => {
   res.render(`register_form`);
 });
 
+
+//POST action that read the inputs and register the user
 app.post("/register", (req, res) => {
   if(!req.body.username || !req.body.password) {
     res.statusCode = 400;
@@ -151,7 +164,6 @@ app.post("/register", (req, res) => {
   } else {
     let email = req.body.username;
     const pass = bcrypt.hashSync(req.body.password, 10);
-    console.log(pass);
     let id = generateId();
     for (var ids in users) {
       if (users.hasOwnProperty(ids)) {
@@ -159,24 +171,21 @@ app.post("/register", (req, res) => {
         if(newUser.email === email) {
           res.statusCode = 400;
           res.redirect("/register");
-        } else {
-          let user = {
-            id: id,
-            email: email,
-            password: pass,
-          }
-          users[id] = user;
-          console.log(users);
-          res.cookie('user_id', user.id);
-          res.redirect(`/urls`);
-        }
+        } 
       }
     }
+    let user = {
+      id: id,
+      email: email,
+      password: pass,
+    }
+    users[id] = user;
+    req.session.user_id =  user.id;
+    res.redirect(`/urls`);
   }
 });
 
-function searchUsername(id) {
-  //console.log(id);
+function searchUser(id) {
   for (var userId in users) {
     if (users.hasOwnProperty(userId)) {
       var user = users[userId];
@@ -215,26 +224,18 @@ function generateRandomString() {
 }
 
 function urlsForUser(id) {
-  debugger;
   let arr = [];
-  console.log(id);
   for (var urls in urlDatabase.urls) {
     if (urlDatabase.urls.hasOwnProperty(urls)) {
       let url = urlDatabase.urls[urls];
-      console.log(url);
       if(url.users.indexOf(id) > -1) {
-        console.log(urls);
-        console.log(url.long);
+        let longUrl = url.long.longURL;
         let link = {
           short: urls,
-          long: url.long
+          long: longUrl
         }
         arr.push(link);
-        console.log(link);
       }
-      // url.users.forEach(function(rId) {
-      //   if(rId === id) arr.push(id);        
-      // });
     }
   }
   return arr;
